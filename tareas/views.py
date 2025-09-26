12f1a9bf2dd0474e86a501f85c521ef551b8a655
Ciro <romano.agustin24@gmail.com>
2025-09-26 19:15:21 -0300

    first commit

---
+++ tareas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Tarea
+# Create your views here.
+
+def lista_tareas(request):
+    
+    tareas = Tarea.objects.all()
+    return render(request, 'tareas/lista_tareas.html/', {'tereas' : tareas })
+
+
